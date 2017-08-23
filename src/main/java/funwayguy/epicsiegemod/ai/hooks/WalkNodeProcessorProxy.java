8a3158d067692e87cf017776cc2c840c553159b2
LongHairedHacker <sebastian@sebastians-site.de>
2017-08-23 21:01:57 +0200

    Fixed addCoord

--- src/main/java/funwayguy/epicsiegemod/ai/hooks/WalkNodeProcessorProxy.java
+++ src/main/java/funwayguy/epicsiegemod/ai/hooks/WalkNodeProcessorProxy.java
@@ -292,7 +292,7 @@ public class WalkNodeProcessorProxy extends WalkNodeProcessor
                         double d3 = (double)(z - facing.getFrontOffsetZ()) + 0.5D;
                         AxisAlignedBB axisalignedbb = new AxisAlignedBB(d2 - d1, (double)y + 0.001D, d3 - d1, d2 + d1, (double)((float)y + this.entity.height), d3 + d1);
                         AxisAlignedBB axisalignedbb1 = blockstate.getBoundingBox(this.blockaccess, blockpos);
-                        AxisAlignedBB axisalignedbb2 = axisalignedbb.addCoord(0.0D, axisalignedbb1.maxY - 0.002D, 0.0D);
+                        AxisAlignedBB axisalignedbb2 = axisalignedbb.offset(0.0D, axisalignedbb1.maxY - 0.002D, 0.0D);
 
                         if (this.entity.world.collidesWithAnyBlock(axisalignedbb2))
                         {
