8eacfba79e58a81e4db4f29628365a4df1325e9d
LongHairedHacker <sebastian@sebastians-site.de>
2017-08-23 21:07:56 +0200

    Ooops missed one

--- src/main/java/funwayguy/epicsiegemod/client/ESMGuiFactory.java
+++ src/main/java/funwayguy/epicsiegemod/client/ESMGuiFactory.java
@@ -13,7 +13,15 @@ public class ESMGuiFactory implements IModGuiFactory
 	{
 	}
 
+	@Override
+	public boolean hasConfigGui() {
+		return false;
+	}
 
+	@Override
+	public GuiScreen createConfigGui(GuiScreen parentScreen) {
+		return null;
+	}
 
 	@Override
 	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
